72781ac65bb4a335c655139554b69062411be076
anironL <viasolaris@gmail.com>
2023-01-23 20:00:42 -0500

    Done exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  # Employees must always have a first name present
+  # Employees must always have a last name present
+  # Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+  validates :first_name, :last_name, :store_id, presence: true
+  # Employees have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  # Stores must always have a name that is a minimum of 3 characters
+  validates :name, length: { minimum: 3 }
+  # Stores have an annual_revenue that is a number (integer) that must be 0 or more
+  validates :annual_revenue, numericality: { only_integer: true } 
+  
+  # BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+  validate :mens_apparel, :womens_apparel
+  def must_sell_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add "Need to sell something."
+    end
+  end
 end
+
+
